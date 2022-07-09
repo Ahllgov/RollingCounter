937830fb6f88ae4c108f5e92f5d988589ae6445a
makhilgov <makhilgov@ozon.ru>
2022-07-09 12:16:13 +0300

    Initial Commit

---
+++ Shared/RollingCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingCounterApp.swift
+//  Shared
+//
+//  Created by Akhilgov Magomed Abdulmazhitovich on 09.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct RollingCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
